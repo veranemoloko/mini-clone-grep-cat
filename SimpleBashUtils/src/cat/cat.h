f466e464e736c24026bb74cb1c40eb316988c1d9
Verochka <133212611+veranemoloko@users.noreply.github.com>
2025-04-13 17:31:14 +0300

    Delete SimpleBashUtils directory

--- SimpleBashUtils/src/cat/cat.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAT_H
-#define CAT_H
-
-#include "../common/resType.h"
-#include "options.h"
-
-Result makeOutput(const int, char **, const Op);
-
-#endif
--- SimpleBashUtils/src/grep/parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSE_H
-#define PARSE_H
-
-#include "../common/resType.h"
-#include "options.h"
-
-Result parseOptions(int, char **, Op *, char **);
-
-#endif
